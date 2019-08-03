aff71efe90a8b505c48b66709077c42dd26169d3
Antonio José Masiá <email@antoniomasia.com>
2019-08-03 17:18:04 +0200

    Update app scripts and tslint config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-// tslint:disable-next-line: no-console
 console.log('App running ...')
 import { add } from 'ramda'
 
@@ -15,7 +14,5 @@ export class Hero {
 }
 
 const hero = new Hero('krunal')
-// tslint:disable-next-line: no-console
 console.log(hero.myName())
-// tslint:disable-next-line: no-console
 console.log('SUM', add(3, 3))
