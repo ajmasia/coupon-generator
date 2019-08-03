f26644c4fc248feff7057f82209cc4d54ba44eb1
Antonio José Masiá <email@antoniomasia.com>
2019-08-03 15:33:52 +0200

    Add initial webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'app.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: {
+    html: new HtmlWebpackPlugin({
+      template: path.join('./src', 'index.html'),
+      filename: './index.html'
+    })
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hotOnly: false,
+    overlay: true,
+    port: 3000,
+    host: 'localhost'
+  }
+}
