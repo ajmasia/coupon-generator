0e3ed6be712cea3586cdbbf619f42534f3290138
Antonio José Masiá <email@antoniomasia.com>
2019-08-04 13:59:01 +0200

    Update secuential algorithm to check max long number

--- src/services/couponsGenerator/index.ts
+++ src/services/couponsGenerator/index.ts
@@ -3,9 +3,10 @@ import { getLargestNum, padNumber } from '../../utils'
 class Coupon {
   // Properties
   private amount: number = 5
+  private long: number = 5
 
   public generate(initValue: number): string[] {
-    const macNumber = getLargestNum(this.amount)
+    const macNumber = getLargestNum(this.long)
 
     if (initValue > macNumber - this.amount + 1) {
       return ['Error: Initial value It is higher than allowed']
@@ -14,7 +15,7 @@ class Coupon {
     const result: string[] = Array.from(Array(this.amount), (x, index) => {
       const num: number = index + initValue
 
-      return padNumber(num.toString(), 5)
+      return padNumber(num.toString(), this.long)
     })
 
     return result
