ef3fcea9921ab83f663215547963e6c7968e0f31
Antonio José Masiá <email@antoniomasia.com>
2019-08-04 12:53:01 +0200

    Delete unused class constructor

--- src/services/couponsGenerator/index.ts
+++ src/services/couponsGenerator/index.ts
@@ -4,10 +4,6 @@ class Coupon {
   // Properties
   private amount: number = 5
 
-  constructor() {
-    // TODO: Read generator config from file
-  }
-
   public generate(initValue: number): string[] {
     const result: string[] = Array.from(Array(this.amount), (x, index) => {
       const num: number = index + initValue
