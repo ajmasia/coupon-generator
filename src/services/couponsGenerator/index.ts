f09a3af89b351da27d444d6252270cdf7b519e78
Antonio José Masiá <email@antoniomasia.com>
2019-08-04 21:40:24 +0200

    Update Copuon class to support new algorithm

--- src/services/couponsGenerator/index.ts
+++ src/services/couponsGenerator/index.ts
@@ -2,13 +2,14 @@ import serviceConfig from './config'
 import { IAlgorithm } from './models/algorithms'
 import algorithms from './utils/Algorithm'
 
-const { amount, digits, initialValue, algorithm } = serviceConfig
+const { amount, digits, initialValue, algorithm, pattern } = serviceConfig
 
 class Coupon {
   // Properties
   private amount: number = amount
   private digits: number = digits
   private initialValue: number = initialValue
+  private pattern: string = pattern
   private algorithm: string = algorithm
   private useAalgorithm: IAlgorithm = algorithms
 
@@ -19,6 +20,12 @@ class Coupon {
           this.initialValue,
           this.digits,
           this.amount
+        )
+      case 'alphanumeric':
+        return this.useAalgorithm.alphanumeric(
+          this.digits,
+          this.amount,
+          this.pattern
         )
       default:
         return this.useAalgorithm.secuential(
